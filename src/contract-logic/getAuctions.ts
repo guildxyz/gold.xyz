8c8fb3b656f7a505b91b638b61ac919db0b877c4
Bruno <>
2021-10-26 15:32:50 +0200

    slugify id

--- src/contract-logic/getAuctions.ts
+++ src/contract-logic/getAuctions.ts
@@ -10,11 +10,18 @@ async function getAuctions() {
   )
   const AuctionPoolAccount = await connection.getAccountInfo(auctionPoolPubkey)
   const AuctionPoolData: Buffer = AuctionPoolAccount!.data
-  return deserializeUnchecked(
-    Layout.AUCTION_POOL_SCHEMA,
-    Layout.AuctionPool,
-    Buffer.from(AuctionPoolData)
-  )
+  let AuctionMap = deserializeUnchecked(Layout.AUCTION_POOL_SCHEMA, Layout.AuctionPool, Buffer.from(AuctionPoolData));
+  
+  let slugMap: any = Array.from((AuctionMap.pool.entries())).map((auction: any) => {  
+    let slugId = Buffer.from(auction[0].split(","))
+    let index = slugId.indexOf(0x00)
+    let unpadded = slugId.slice(0, index).toString();
+    return {
+      name: unpadded,
+      id: auction[1]
+    }
+  })  
+  return slugMap
 }
 
 export default getAuctions
