37407a4ba6e3330d3a2bfc1e9daf2585994d1955
valid <valid@zgen.hu>
2021-11-14 17:22:38 +0100

    removed delete auction option

--- src/components/[auction]/SettingsMenu.tsx
+++ src/components/[auction]/SettingsMenu.tsx
@@ -10,8 +10,7 @@ import { useConnection, useWallet } from "@solana/wallet-adapter-react"
 import { freeze } from "contract-logic/transactions/freeze"
 import useSubmit from "hooks/useSubmit"
 import useToast from "hooks/useToast"
-import { useRouter } from "next/router"
-import { Gear, Snowflake, Trash } from "phosphor-react"
+import { Gear, Snowflake } from "phosphor-react"
 import { useSWRConfig } from "swr"
 import useAuction from "./hooks/useAuction"
 
@@ -20,7 +19,6 @@ const SettingsMenu = () => {
   const { connection } = useConnection()
   const { sendTransaction } = useWallet()
   const toast = useToast()
-  const router = useRouter()
   const { mutate } = useSWRConfig()
 
   const handleFreezeAuction = async () => {
@@ -52,36 +50,6 @@ const SettingsMenu = () => {
       }),
   })
 
-  const handleDeleteAuction = async () => {
-    throw new Error("Not implemented yet")
-    // const tx = await deleteAuction(connection, auction?.ownerPubkey, auction?.id)
-    // console.log(tx)
-    // const signature = await sendTransaction(tx, connection, {
-    //   skipPreflight: false,
-    //   preflightCommitment: "singleGossip",
-    // })
-    // console.log("info", "Transaction sent:", signature)
-    // await connection.confirmTransaction(signature, "processed")
-    // console.log("success", "Transaction successful!", signature)
-  }
-
-  const { onSubmit: onDelete } = useSubmit(handleDeleteAuction, {
-    onSuccess: () => {
-      toast({
-        title: `Auction successfully deleted!`,
-        status: "success",
-      })
-      mutate("auctions")
-      router.push("/")
-    },
-    onError: (e) =>
-      toast({
-        title: "Error deleting auction",
-        description: e.toString(),
-        status: "error",
-      }),
-  })
-
   return (
     <Menu placement="bottom-end">
       <MenuButton as={IconButton} aria-label="Auction settings" h="10">
@@ -90,9 +58,6 @@ const SettingsMenu = () => {
       <MenuList border="none" shadow="md">
         <MenuItem py="2" icon={<Snowflake />} onClick={onFreeze}>
           Freeze auction
-        </MenuItem>
-        <MenuItem py="2" icon={<Trash />} onClick={onDelete} color="red.400">
-          Delete auction
         </MenuItem>
       </MenuList>
     </Menu>
