c88f67ea4f7a783d7feb06b211fc8881f2552a8d
valid <valid@zgen.hu>
2021-11-14 19:10:19 +0100

    fetching SOL price from api

--- src/components/[auction]/HighestBid.tsx
+++ src/components/[auction]/HighestBid.tsx
@@ -1,12 +1,19 @@
 import { StatHelpText, StatNumber } from "@chakra-ui/stat"
 import useSWR from "swr"
+import fetcher from "utils/fetcher"
 
 type Props = {
   amount: number
 }
 
+const fetchPrice = (endpoint) => fetcher(endpoint).then((res) => res?.solana?.usd)
+
 const HighestBid = ({ amount }: Props) => {
-  const { data: solPrice } = useSWR("/api/sol-price")
+  const { data: solPrice } = useSWR(
+    "https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd",
+    fetchPrice,
+    { dedupingInterval: 10000 }
+  )
 
   if (!amount)
     return (
