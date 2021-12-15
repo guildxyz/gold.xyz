f65ba73e675ade3af78ede11d73e89aebe50294d
valid <valid@zgen.hu>
2021-12-15 02:20:29 +0100

    NumberOfCycles checkbox to create infinite auction

--- src/components/create-auction/NumberOfCycles.tsx
+++ src/components/create-auction/NumberOfCycles.tsx
@@ -1,5 +1,12 @@
-import { FormControl, FormErrorMessage, Input } from "@chakra-ui/react"
-import { useEffect, useMemo } from "react"
+import {
+  Checkbox,
+  FormControl,
+  FormErrorMessage,
+  Input,
+  Stack,
+  Text,
+} from "@chakra-ui/react"
+import { useEffect, useMemo, useState } from "react"
 import { useFormContext, useWatch } from "react-hook-form"
 
 const NumberOfCycles = () => {
@@ -9,12 +16,19 @@ const NumberOfCycles = () => {
     trigger,
     formState: { errors, dirtyFields },
   } = useFormContext()
+  const [isInfinite, setIsInfinite] = useState()
 
   const nfts = useWatch({ name: "nfts" })
   const maxSupply = useMemo(
     () => (Object.keys(nfts).length <= 1 ? undefined : Object.keys(nfts).length),
     [nfts]
   )
+
+  const handleChange = (e) => {
+    const isChecked = e.target.checked
+    setIsInfinite(isChecked)
+    if (isChecked) setValue("numberOfCycles", undefined)
+  }
 
   useEffect(() => {
     if (!maxSupply) return
@@ -23,39 +37,37 @@ const NumberOfCycles = () => {
   }, [maxSupply, setValue, dirtyFields])
 
   return (
-    <FormControl isInvalid={errors?.numberOfCycles} isRequired>
-      {/* <FormLabel>Number of rounds</FormLabel> */}
-      <Input
-        {...register("numberOfCycles", {
-          required: "This field is required.",
-          valueAsNumber: true,
-          max: {
-            value: maxSupply,
-            message: `Can't exceed max NFT supply (${maxSupply})`,
-          },
-        })}
-        size="lg"
-        maxW="sm"
-        placeholder="0"
-        type="number"
-      />
-      {/* <NumberInput size="lg" maxW="sm">
-        <NumberInputField
+    <FormControl
+      isInvalid={errors?.numberOfCycles}
+      isRequired={!isInfinite}
+      isDisabled={isInfinite}
+    >
+      <Stack
+        direction={{ base: "column", md: "row" }}
+        spacing={{ base: 4, md: 8 }}
+        alignItems={{ md: "center" }}
+      >
+        <Input
           {...register("numberOfCycles", {
-            required: "This field is required.",
+            required: !isInfinite && "This field is required.",
             valueAsNumber: true,
             max: {
               value: maxSupply,
-              message: "Can't exceed max NFT supply",
+              message: `Can't exceed max NFT supply (${maxSupply})`,
             },
           })}
           placeholder="0"
-        /> */}
-      {/* <NumberInputStepper>
-          <NumberIncrementStepper />
-          <NumberDecrementStepper />
-        </NumberInputStepper> */}
-      {/* </NumberInput> */}
+          size="lg"
+          maxW={{ base: "2xs", lg: "xs" }}
+        />
+        <Text colorScheme="gray" fontWeight={"bold"}>
+          OR
+        </Text>
+
+        <Checkbox checked={isInfinite} onChange={handleChange}>
+          Infinite auction
+        </Checkbox>
+      </Stack>
       <FormErrorMessage>{errors?.numberOfCycles?.message}</FormErrorMessage>
     </FormControl>
   )
