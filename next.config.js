a708d459f291db9b744a8e30eaacf021b8c06263
Mark Melczer <melczer7@gmail.com>
2022-01-13 08:28:49 +0100

    Revert patch.

--- next.config.js
+++ next.config.js
@@ -37,6 +37,48 @@ module.exports = withTM({
       )
     }
 
+    // From https://github.com/vercel/next.js/issues/22581#issuecomment-864476385
+    const ssrPlugin = config.plugins.find((plugin) => plugin instanceof SSRPlugin)
+
+    if (ssrPlugin) {
+      patchSsrPlugin(ssrPlugin)
+    }
+
     return config
   },
 })
+
+// Patch the NextJsSSRImport plugin to not throw with WASM generated chunks.
+function patchSsrPlugin(plugin) {
+  plugin.apply = function apply(compiler) {
+    compiler.hooks.compilation.tap("NextJsSSRImport", (compilation) => {
+      compilation.mainTemplate.hooks.requireEnsure.tap(
+        "NextJsSSRImport",
+        (code, chunk) => {
+          // The patch that we need to ensure this plugin doesn't throw
+          // with WASM chunks.
+          if (!chunk.name) {
+            return
+          }
+
+          // Update to load chunks from our custom chunks directory
+          const outputPath = resolve("/")
+          const pagePath = join("/", dirname(chunk.name))
+          const relativePathToBaseDir = relative(pagePath, outputPath)
+          // Make sure even in windows, the path looks like in unix
+          // Node.js require system will convert it accordingly
+          const relativePathToBaseDirNormalized = relativePathToBaseDir.replace(
+            /\\/g,
+            "/"
+          )
+          return code
+            .replace('require("./"', `require("${relativePathToBaseDirNormalized}/"`)
+            .replace(
+              "readFile(join(__dirname",
+              `readFile(join(__dirname, "${relativePathToBaseDirNormalized}"`
+            )
+        }
+      )
+    })
+  }
+}
