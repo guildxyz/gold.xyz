68f63d027f24106daa3276939d136f102359295b
valid <valid@zgen.hu>
2022-01-20 14:34:32 +0100

    added default minimumBidAmount

--- src/components/create-auction/hooks/useStartAuction.ts
+++ src/components/create-auction/hooks/useStartAuction.ts
@@ -78,6 +78,7 @@ const useStartAuction = () => {
       })
       const finalData = {
         ..._data,
+        minimumBidAmount: 0.05,
         cyclePeriod:
           (_data.cyclePeriod === "CUSTOM"
             ? _data.customCyclePeriod
