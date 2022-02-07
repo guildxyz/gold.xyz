4b26d9aea925870e8c40da4e7fc7ec13450ec723
valid <valid@zgen.hu>
2022-02-07 17:59:15 +0100

    new simplified progressbar

--- src/components/[auction]/ProgressBar.tsx
+++ src/components/[auction]/ProgressBar.tsx
@@ -1,15 +1,11 @@
-import { HStack, Progress, Skeleton, Tag, Text, VStack } from "@chakra-ui/react"
+import { HStack, Progress, Text, VStack } from "@chakra-ui/react"
 import { useMemo } from "react"
 import { Rest } from "types"
-import Countdown from "./Countdown"
 import useAuction from "./hooks/useAuction"
-import useCycle from "./hooks/useCycle"
 
 const ProgressBar = ({ ...rest }: Rest): JSX.Element => {
   const { auction } = useAuction()
   const { goalTreasuryAmount, allTimeTreasuryAmount } = auction || {}
-  const { cycle } = useCycle()
-  const { bids, endTimestamp } = cycle ?? {}
 
   const percentage = useMemo(
     () => (allTimeTreasuryAmount * 100) / goalTreasuryAmount,
@@ -19,41 +15,27 @@ const ProgressBar = ({ ...rest }: Rest): JSX.Element => {
   return (
     <>
       {goalTreasuryAmount && (
-        <VStack width="full" spacing={2} {...rest}>
-          <HStack justifyContent="space-between" width="full">
-            <Text as="span" fontWeight="bold">
-              {allTimeTreasuryAmount}
-              <Text as="span" fontSize="xs">
-                {" SOL"}
+        <VStack spacing={2} w="full">
+          <HStack justifyContent="space-between" width="full" px="2px">
+            <Text fontSize="xs">
+              <Text as="span" fontWeight="extrabold" fontSize="sm">
+                {allTimeTreasuryAmount}
               </Text>
+              {" SOL raised"}
             </Text>
-
-            <HStack>
-              <Tag size="sm">{bids?.length || 0}</Tag>
-              <Text as="span" fontSize="sm">{` bid${
-                bids?.length > 1 ? "s" : ""
-              }`}</Text>
-            </HStack>
+            <Text fontSize="xs">
+              <Text as="span" fontWeight="bold" fontSize="sm">
+                {`${percentage.toFixed(2)}%`}
+              </Text>
+              {` of ${goalTreasuryAmount} SOL goal`}
+            </Text>
           </HStack>
-
           <Progress
-            hasStripe
             colorScheme="orange"
             rounded="md"
             width="full"
             value={percentage}
           />
-
-          <HStack justifyContent="space-between" width="full" fontSize="sm">
-            <Text as="span">
-              <Text as="span" fontWeight="bold">{`${percentage.toFixed(2)}%`}</Text>
-              {` of ${goalTreasuryAmount} SOL goal`}
-            </Text>
-
-            <Skeleton isLoaded={!!endTimestamp}>
-              <Countdown expiryTimestamp={endTimestamp} simple />
-            </Skeleton>
-          </HStack>
         </VStack>
       )}
     </>
